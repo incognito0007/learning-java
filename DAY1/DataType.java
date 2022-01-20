293ffd9d1e8c51c25209b6c845100c3dbc7e87d9
Incognito <89413148+incognito0007@users.noreply.github.com>
2022-01-20 21:19:40 +0530

    Add files via upload

---
+++ DAY1/DataType.java
@@ -0,0 +1,13 @@
+package DAY1;
+public class DataType {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        // int diff = b-a;
+        // System.out.println(diff);
+        // int mul = a * b;
+        // System.out.println(mul);
+    }
+}
